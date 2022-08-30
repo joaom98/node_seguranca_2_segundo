6d8b87b8c8cbd158e47e83f0ea074d3cee535f47
joaom98 <joaomanoellima98@gmail.com>
2022-08-30 20:11:58 -0300

    Faltou no arquivo inicial do projeto

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import UsuariosController from '../controllers/usuariosController.js';
+import estrategia from '../middlewares/autenticacao.js';
 
 const router = express.Router();
 
 router
   .post('/usuario', UsuariosController.cadastrarUsuario)
-  .delete('/usuario/:id', UsuariosController.excluirUsuario);
+  .delete('/usuario/:id',,UsuariosController.excluirUsuario)
+  .post('/login',, UsuariosController.login);
 
 export default router;
